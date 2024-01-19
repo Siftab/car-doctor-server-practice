8b794be897aeb3527c7e0e9cdd397bbca8c24cfb
Siftab <siiffuuu@gmail.com>
2024-01-20 01:23:48 +0600

    update

---
+++ _basic.js
@@ -0,0 +1,8 @@
+/**
+ * jokhn successfully login hocche tokhn jwt api hit kore token create kortese then token take cookies e set kortese with httpony set credentials and samesite 
+ * 
+ * 
+ * er por jokhn logout kortese ,
+ * tkhn seita else statement er moddhe 
+ * logOut er Api hit kortese jeta mainly cokkies take clear kore dicche 
+ * and ei total ka ta front end er authProvider er Auth observer moddhe hocche */ 
