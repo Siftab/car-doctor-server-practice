1cd865606126c91e3b31f42d82ce281c7a5160a1
Siftab <siiffuuu@gmail.com>
2024-01-24 16:01:54 +0600

    cors updated

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors({
-     origin:["http://localhost:5173"],
+     origin:["http://localhost:5173","https://car-doctor-a2231.web.app/"],
      credentials:true}));
 app.use(express.json());
 app.use(cookieParser())
@@ -54,7 +54,7 @@ const verifyToken=(req,res,next)=>{
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         const serviceCollection = client.db('car-doctor').collection('services');
         const bookingCollection = client.db('car-doctor').collection('bookings');
@@ -141,7 +141,7 @@ async function run() {
 
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
